cf0edf88a8c258cc8fcc10de4cf42d1e40de69cd
Denis Wirries <denis@wirries.me>
2018-12-19 21:33:45 +0100

    auto refresh images

--- camera-app/src/app/browse/browse.component.ts
+++ camera-app/src/app/browse/browse.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {ApiService} from '../api.service';
 import {ImageModel} from '../model/image.model';
 import {ImageDetailModel} from '../model/image-detail.model';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.css']
 })
-export class BrowseComponent implements OnInit {
+export class BrowseComponent implements OnInit, OnDestroy {
+
+  timer = null;
 
   images: ImageModel[];
   details: ImageDetailModel[];
@@ -21,13 +24,22 @@ export class BrowseComponent implements OnInit {
 
   ngOnInit() {
     this._api.getImages().subscribe(images => this.images = images);
+
+    const interval1 = interval(15000); // update every 15s
+    this.timer = interval1.subscribe(n => {
+      this._api.getImages().subscribe(images => this.images = images);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer != null) {
+      this.timer.unsubscribe();
+    }
   }
 
   onSelect($event: MouseEvent, image: ImageModel) {
     this.selectedImage = image;
     this._api.getImageDetails(image.id).subscribe(details => this.details = details);
   }
-
-  // TODO AutoRefresh images
 
 }
