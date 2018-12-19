7dd25dc94918ab3a0142efbac97d9f4c576b9796
Denis Wirries <denis@wirries.me>
2018-12-19 12:43:18 +0100

    build without docker

--- build.sh
+++ build.sh
@@ -4,37 +4,16 @@
 #
 # User must have access to Docker.
 #
+# Usage: ./build.sh [-skipDocker]
+#  - with -skipDocker you can skip the Docker build
+# 
 
 WORK_DIR=$(pwd)
 
 # Build the camera-service
 cd $WORK_DIR/camera-service
-./build.sh
+./build.sh $1
 
 # Build the camera-app
 cd $WORK_DIR/camera-app
-./build.sh
-
-#NAME=camera-app
-#TAG=LATEST
-
-# Remove old build
-#echo Remove old build
-#rm -rf dist
-
-# Building the app
-#echo Building the app
-#npm install
-#ng build --prod
-
-# Remove unused images
-#echo Remove unused images
-#docker image prune -a -f
-
-# Building the image
-#echo Building the image
-#docker build -t $NAME:$TAG .
-
-# Running the container
-#echo Running the container on port 8888
-#docker run --rm -it -p 8888:80 $NAME:$TAG 
+./build.sh $1
